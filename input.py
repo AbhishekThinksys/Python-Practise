b172e4df3c99f21ac8d32e2622ef4726b203f4e7
AbhishekThinksys <kapil.abhishek@thinksys.com>
2023-09-12 13:57:50 +0530

    python

--- input.py
+++ input.py
@@ -1,4 +1,8 @@
 # name = input("enter your name ")
 # print("My name is " + name)
-age = input("enter the age ")
-print("my age is " + age)
+#age = input("enter the age ")
+#print("my age is " + age)
+first_number = int(input("Enter first_number "))
+second_number = int(input("Enter second_number "))
+total = first_number + second_number
+print("Number is " + str(total))
