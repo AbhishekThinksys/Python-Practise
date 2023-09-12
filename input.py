809c02c75c5983facb41df767e1e5c7a7401fb3f
AbhishekThinksys <kapil.abhishek@thinksys.com>
2023-09-12 14:48:35 +0530

    python

--- input.py
+++ input.py
@@ -2,7 +2,28 @@
 # print("My name is " + name)
 #age = input("enter the age ")
 #print("my age is " + age)
-first_number = int(input("Enter first_number "))
-second_number = int(input("Enter second_number "))
-total = first_number + second_number
-print("Number is " + str(total))
+# first_number = int(input("Enter first_number "))
+# second_number = int(input("Enter second_number "))
+# total = first_number + second_number
+# print("Number is " + str(total))
+# name , age = "abhishek","22"
+# print("Hi " + name + " your age is " + age)
+# x=y=z=1
+# print(x)
+# print(y)
+# print(z)
+# print(x+y+z)
+#name = input("enter your name ")
+#age = input("enter your age ")
+# name,age = input("Enter your name and age ").split()
+#name,age = input("Enter your name and age ").split(",")
+# print(name)
+# print(age)
+#name ="abhishek"
+#age = 22
+name = input("enter your name")
+age = input ("your age")
+#name,age = input("enter your name and your age ").split()
+#print("hello {} your age is {}".format(name,age)) # this is for python 3 version
+print(f"hello {name} your age is {age}")   # this is latest method for string formatting
+
